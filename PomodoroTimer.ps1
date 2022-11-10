8e13c6e874c4444b728c0861401ae29c818dee35
Glenn Toms <Glenn@glenntoms.gg>
2022-11-10 13:41:16 +0000

    Added Comment Based Help

--- PomodoroTimer.ps1
+++ PomodoroTimer.ps1
@@ -1,21 +1,51 @@
 function Start-Pomodoro {
+    <#
+        .SYNOPSIS
+        Powershell based Pomodoro Timer with 3 short breaks and 1 long break before reseting
+        
+        .DESCRIPTION
+        Powershell based Pomodoro Timer with 3 short breaks and 1 long break before reseting
+
+        .PARAMETER Pomodoro
+        Specifies the length of time for working
+
+        .PARAMETER ShortBreak
+        Specifies the length of time for a short break
+        
+        .PARAMETER LongBreak
+        Specifies the length of time for a long break
+
+        .PARAMETER DisableNotifications
+        Disables Windows Toast Notifications
+
+        .EXAMPLE
+        PS> Start-Pomodoro -Pomodoro 20 -ShortBreak 3 -LongBreak 10 -DisableNotifications
+        Set work time to 20 minutes, short breaks at 3 minutes, a long break at 10 minutes, and disabled notifications
+
+        .EXAMPLE
+        PS> Start-Pomodoro -DisableNotifications
+        Uses defaul timings and disabled notifications
+
+        .LINK
+        https://github.com/GlennToms/Powershell-Pomodoro-Timer
+    #>
     [CmdletBinding()]
     param (
         [Parameter()]
         [Int]
-        # $Pomodoro = 25,
-        $Pomodoro = 1,
+        $Pomodoro = 25,
         [Parameter()]
         [Int]
-        # $ShortBreak = 5,
-        $ShortBreak = 2,
+        $ShortBreak = 5,
         [Parameter()]
         [Int]
-        # $LongBreak = 15,
-        $LongBreak = 3,
+        $LongBreak = 15,
         [Parameter()]
         [String]
-        $PlayPauseKey = "SpaceBar"
+        $PlayPauseKey = "SpaceBar",
+        [Parameter()]
+        [switch]
+        $DisableNotifications
     )
     $IsBreak = $false
     $SessionCount = 0
@@ -53,11 +83,14 @@ function Start-Pomodoro {
                 Test-KeyPressed -Key $PlayPauseKey
             }
         }
-        if ($IsBreak) {
-            Show-Notification -ToastTitle 'PomodoroTimer' -ToastText "Deep work ended"
-        }
-        else {
-            Show-Notification -ToastTitle 'PomodoroTimer' -ToastText "Break ended"
+        
+        if ($DisableNotifications -eq $false) {
+            if ($IsBreak) {
+                Show-ToastNotification -ApplicationTitle 'PomodoroTimer' -Text "Deep work ended"
+            }
+            else {
+                Show-ToastNotification -ApplicationTitle 'PomodoroTimer' -Text "Break ended"
+            }
         }
     }
 }
@@ -113,14 +146,48 @@ function Test-KeyPressed {
     }
 }
 
-function Show-Notification {
+function Show-ToastNotification {
+    <#
+        .SYNOPSIS
+        Creates custom Windows Toast notifications with title and text
+        
+        .DESCRIPTION
+        Creates custom Windows Toast notifications with title and text
+
+        .PARAMETER ApplicationTitle
+        Specifies the title of the application that created the Toast notification
+
+        .PARAMETER ToastTitle
+        Specifies the title of the Toast notification 
+
+        .PARAMETER ToastBody
+        Specifies the text of the Toast notification 
+        
+        .EXAMPLE
+        PS> Show-ToastNotification -Title 'PomodoroTimer' -Text "Deep work ended"
+        
+        .EXAMPLE
+        PS> "This is body text" | Show-ToastNotification
+
+        .LINK
+        https://github.com/GlennToms/Powershell-Pomodoro-Timer
+
+        .LINK
+        https://den.dev/blog/powershell-windows-notification/
+
+        .NOTES
+        Updated for use with Pomodoro Timer
+
+    #>
     [cmdletbinding()]
     Param (
         [string]
+        $ApplicationTitle = "Powershell",
+        [string]
         $ToastTitle,
         [string]
-        [parameter(ValueFromPipeline)]
-        $ToastText
+        [parameter(ValueFromPipeline, Mandatory = $true)]
+        $ToastBody
     )
 
     [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
@@ -128,18 +195,18 @@ function Show-Notification {
 
     $RawXml = [xml] $Template.GetXml()
     ($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "1" }).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
-    ($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "2" }).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+    ($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "2" }).AppendChild($RawXml.CreateTextNode($ToastBody)) > $null
 
     $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
     $SerializedXml.LoadXml($RawXml.OuterXml)
 
     $Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)
-    $Toast.Tag = "PowerShell"
-    $Toast.Group = "PowerShell"
+    $Toast.Tag = $ApplicationTitle
+    $Toast.Group = $ApplicationTitle
     $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
 
-    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
+    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($ApplicationTitle)
     $Notifier.Show($Toast);
 }
 
-Start-Pomodoro
+# Start-Pomodoro
